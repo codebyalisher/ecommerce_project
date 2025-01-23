5254cbceb585b6e3e7d658064b5a4cc6a392e955
Ali Sher - Software Engineer and Developer <alisher.201903953@gcuf.edu.pk>
2025-01-23 14:02:16 +0500

    Update settings.py

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -41,9 +41,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'alisher3316374@gmail.com'
-EMAIL_HOST_PASSWORD = 'ALIsher137669#'
-DEFAULT_FROM_EMAIL = 'alisher3316374@gmail.com'
+EMAIL_HOST_USER = 'youruser@gmail.com'
+EMAIL_HOST_PASSWORD = 'yourpassword#'
+DEFAULT_FROM_EMAIL = 'youranothermail@gmail.com'
 
 
 INSTALLED_APPS = [
